bc5f83f8599a6ae0e1a5f3bb44a7dab242202b43
Phill Conrad <pconrad@cs.ucsb.edu>
2025-08-12 12:54:53 -0700

    pc - try a dummy test

---
+++ frontend/src/sum.test.js
@@ -0,0 +1,11 @@
+import { describe, expect, it } from 'vitest'
+
+function sum(a, b) {
+    return a + b
+}
+
+describe('sum', () => {
+    it('adds 1 + 2 to equal 3', () => {
+        expect(sum(1, 2)).toBe(3)
+    })
+})
